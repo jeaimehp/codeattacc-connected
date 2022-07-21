0d9c56bcf359140c72c549553d9820e641e72c16
Je'aime Powell <jeaimehp@gmail.com>
2022-07-20 19:33:48 -0500

    cleaned site dir

--- _site/individual-scripts/jupyter-setup.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yes|pip3 install notebook
-yes|pip3 install jupyter
-
-echo "To start jupyter notebooks type \"jupyter notebook\" "
--- _site/sensortest.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CYAN='\033[0;36m'
-PURPLE='\033[0;35m'
-NC='\033[0m' # No Color
-printf "${PURPLE}Running Pimoroni Enviro+ Sensor Test....\n"
-echo " "
-printf "${CYAN}Press Ctrl + c to stop the test\n"
-python3 ~/codeattacc-connected/enviroplus-python/examples/weather-and-light.py
